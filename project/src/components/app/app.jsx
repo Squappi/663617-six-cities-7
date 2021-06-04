d4435da56c09598e65f4bceda342639f8204c36b
Squappi <69323875+Squappi@users.noreply.github.com>
2021-06-04 21:31:16 +0300

    module1-task3

--- project/src/components/app/app.jsx
+++ project/src/components/app/app.jsx
@@ -1,7 +1,14 @@
 import React from 'react';
+import MainComponent from '../main/main';
+import PropTypes from 'prop-types';
 
-function App() {
-  return <p>Hello, world!</p>;
+function App(props) {
+  const { cards } = props;
+  return <MainComponent cards = {cards} />;
 }
+
+App.propTypes = {
+  cards: PropTypes.number.isRequired,
+};
 
 export default App;
