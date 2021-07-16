1b690397102c913d93e87f6463d1875070c9bdf4
Squappi <69323875+Squappi@users.noreply.github.com>
2021-07-16 22:21:46 +0300

    consultation

--- project/src/components/property/property.jsx
+++ project/src/components/property/property.jsx
@@ -126,16 +126,16 @@ function PropertyComponent(props) {
                   </p>
                 </div>
               </div>
-              {authorizationStatus === AuthorizationStatus.AUTH &&
-                comments ?
+              {comments ?
                 <section className="property__reviews reviews">
                   <h2 className="reviews__title">Reviews &middot; <span className="reviews__amount">{comments.length}</span></h2>
                   <ul className="reviews__list">
                     {comments.map((reviews) => <CommentComponent key={reviews.id} reviews={reviews}/>)}
                   </ul>
-                  <FormComponent card={card} comments={comments}/>
+                  {authorizationStatus === AuthorizationStatus.AUTH ?
+                    <FormComponent card={card} comments={comments}/> : <LoadingScreen/>}
                 </section>
-                : <LoadingScreen/>}
+                : ''}
             </div>
           </div>
           <section className="property__map map">
